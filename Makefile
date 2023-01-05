28f4e60dd43003a168e86a5d7a826d15e6342822
Alberto Lerda <30939098+albertolerda@users.noreply.github.com>
2023-01-05 11:31:35 +0100

    feat: 💫 restroom-test (#3)
    * feat: 💫 initial version restroom-test
    
    * some cleaning
    
    * feat: add scheme flags (http/https)
    
    * outdated comment

--- Makefile
+++ Makefile
@@ -2,6 +2,7 @@ all: zendebug zenexplorer
 
 zenexplorer_sources := main.go zencodeStatements.go delegate.go zenroom.go
 zendebug_sources := main.go
+restroom_test_sources := main.go
 
 zenexplorer: $(wildcard src/zenexplorer/*.go)
 	go build -o zenexplorer $(foreach source, ${zenexplorer_sources}, src/zenexplorer/${source})
@@ -9,5 +10,8 @@ zenexplorer: $(wildcard src/zenexplorer/*.go)
 zendebug: $(wildcard src/zendebug/*.go)
 	go build -o zendebug $(foreach source, ${zendebug_sources}, src/zendebug/${source})
 
+restroom-test: $(wildcard src/restroom-test/*.go)
+	go build -o restroom-test $(foreach source, ${restroom_test_sources}, src/restroom-test/${source})
+
 clean:
-	rm -f zendebug zenexplorer
+	rm -f zendebug zenexplorer restroom-test
---
+++ src/restroom-test/main.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+type Args struct {
+	scheme string
+	host string
+	port int
+	url  string
+	data string
+}
+
+type ZenroomResult struct {
+	Result string `json:"result"`
+	Logs   string `json:"logs"`
+}
+
+// Warning: names of properties must start with upper caser
+// otherwise JSON Unmarshal doesn't work without errors
+type RestroomError struct {
+	ZenroomError ZenroomResult `json:"zenroom_errors"`
+	Result       string        `json:"result"`
+	Exception    string        `json:"exception"`
+}
+
+type RestroomResult struct {
+	ZenroomResult ZenroomResult `json:"zenroom_errors"`
+	Result        string        `json:"result"`
+	Exception     string        `json:"exception"`
+}
+
+func (args *Args) loadCli() {
+	// flags declaration using flag package
+	flag.StringVar(&args.url, "u", "", "Path to contract.")
+	flag.StringVar(&args.scheme, "s", "http", "Restroom scheme (http/https)")
+	flag.StringVar(&args.host, "h", "127.0.0.1", "Restroom host.")
+	flag.IntVar(&args.port, "p", 5000, "Restroom port.")
+	flag.StringVar(&args.data, "a", "", "Path to data file.")
+
+	flag.Parse() // after declaring flags we need to call it
+}
+
+func (args Args) requestUrl() string {
+	return fmt.Sprintf("%s://%s:%d/api/%s", args.scheme, args.host, args.port, args.url)
+}
+
+// Example usage: ./restroom-test -p 12001 -u sandbox/did-document-create -a data.json
+func main() {
+	var args Args
+	var err error
+	var resp *http.Response
+	args.loadCli()
+
+	log.Printf("Testing endpong: %s\n", args.requestUrl())
+	if args.data == "" {
+		resp, err = http.Get(args.requestUrl())
+	} else {
+		data, err := os.Open(args.data)
+		keys := make(map[string]interface{})
+		if err != nil {
+			fmt.Printf("Error while opening %s: %s", args.data, err)
+			return
+		}
+		defer data.Close()
+		dataBytes, _ := io.ReadAll(data)
+		datakeys := make(map[string]interface{})
+		dataMap := make(map[string]interface{})
+		err = json.Unmarshal(dataBytes, &dataMap)
+		if err != nil {
+			fmt.Printf("Could not read json in file: %s\n", err.Error())
+		}
+		datakeys["data"] = dataMap
+		datakeys["keys"] = keys
+		datakeysBytes, err := json.Marshal(datakeys)
+
+		log.Printf("Data in file: %s\n", args.data)
+		resp, err = http.Post(args.requestUrl(), "application/json",
+			bytes.NewReader(datakeysBytes))
+	}
+	if err != nil {
+		fmt.Printf("Error in the request: %s", err)
+	} else if resp.StatusCode == 500 {
+		rr := RestroomError{}
+		body, _ := io.ReadAll(resp.Body)
+		err = json.Unmarshal([]byte(body), &rr)
+		if err != nil {
+			fmt.Println("Error:")
+			fmt.Println(err)
+		}
+		fmt.Println("==== Zenroom result ====")
+		fmt.Println(rr.ZenroomError.Result)
+		fmt.Println("==== Zenroom logs ====")
+		fmt.Println(rr.ZenroomError.Logs)
+		fmt.Println("==== Restrooom exceptions  ====")
+		fmt.Println(rr.Exception)
+		fmt.Println("==== Restrooom result  ====")
+		fmt.Println(rr.Result)
+	} else if resp.StatusCode == 200 {
+		//var rr map[string]string
+		body, _ := io.ReadAll(resp.Body)
+		fmt.Println(string(body))
+		//_ = json.Unmarshal(body, &rr)
+		/*fmt.Println("Result")
+		fmt.Println(rr["result"])
+
+		fmt.Println("Logs")
+		fmt.Println(rr["result"])*/
+	} else {
+		fmt.Printf("Received status code %d in response", resp.StatusCode)
+	}
+
+	return
+}
